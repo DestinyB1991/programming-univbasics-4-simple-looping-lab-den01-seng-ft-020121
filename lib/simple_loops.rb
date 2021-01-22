df22a8f2aaab620ae1c7383cc258c81603c4b66a
Destiny Boling <bolingdestinyr@gmail.com>
2021-01-22 19:19:12 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,3 +5,11 @@ while count < 5 do
   count += 1
 end
 end
+
+def loop_message_n_times (message, limit)
+  count = 0
+  while count < limit do
+    puts "Hello Moon.", "Hello Red Balloon."
+    count += 1
+  end
+end
