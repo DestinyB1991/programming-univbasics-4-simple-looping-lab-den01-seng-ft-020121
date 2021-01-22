6c34040e3c908f6702abde39ad8c5386c37a9f2c
Destiny Boling <bolingdestinyr@gmail.com>
2021-01-22 19:07:00 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,22 @@
-# Write your methods here
+def loop_message_five_times (message)
+count = 0
+while count < 5 do
+  puts "Hello World."
+  count += 1
+end
+end
+
+
+def loop_message_n_times (message, number)
+5.times {puts "Hello Moon."}
+10.times {puts "Hello Red Balloon."}
+end
+
+def output_array (message, number)
+counter = 0
+array = ["Be yourself;", "everyone else is already taken", "- Oscar Wilde"]
+while array[counter] do
+  puts array[counter]
+  counter += 1
+end
+end
